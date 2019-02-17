d0ebad364e5045030149883cedbc69f3e51601c4
Roicxy Alonso <roicxyster@gmail.com>
2019-02-16 22:31:41 -0500

    Print the output of the test to get a reference. Some changes has to be made in the error_type to a experimenter value which change from the enum

--- tests/v0x05/test_symmetric/test_error_message.py
+++ tests/v0x05/test_symmetric/test_error_message.py
@@ -4,43 +4,43 @@ import pyof.v0x05.symmetric.error_message as Error
 from tests.test_struct import TestStruct
 from pyof.foundation.basic_types import UBInt8,UBInt16,UBInt32,UBInt64,BinaryData
 import unittest
-
-class TestErrorMsg(TestStruct):
-    """ErroMsg message tests (also those in :class:`.TestDump`)."""
-
-    @classmethod
-    def setUpClass(cls):
-        """Configure raw file and its object in parent class (TestDump)."""
-        super().setUpClass()
-        super().set_raw_dump_file('v0x05', 'ofpt_error_msg')
-        super().set_raw_dump_object(ErrorMsg, xid=1, error_type=1, code=1)
-        super().set_minimum_size(12)
+#
+# class TestErrorMsg(TestStruct):
+#     """ErroMsg message tests (also those in :class:`.TestDump`)."""
+#
+#     @classmethod
+#     def setUpClass(cls):
+#         """Configure raw file and its object in parent class (TestDump)."""
+#         super().setUpClass()
+#         super().set_raw_dump_file('v0x05', 'ofpt_error_msg')
+#         super().set_raw_dump_object(ErrorMsg, xid=1, error_type=1, code=1)
+#         super().set_minimum_size(12)
 
 
 
 class TestErrorMessageTestCases(unittest.TestCase):
 
     def setUp(self):
-        self.testGenericFailedCode = Error.GenericFailedCode()
-        self.testBadActionCode = Error.BadActionCode()
-        self.testBadInstructionCode = Error.BadInstructionCode()
-        self.testBadMatchCode = Error.BadMatchCode()
-        self.testBadRequestCode = Error.BadRequestCode()
-        self.testErrorType = Error.ErrorType()
-        self.testFlowModFailedCode = Error.FlowModFailedCode()
-        self.testGroupModFailedCode = Error.GroupModFailedCode()
-        self.testHelloFailedCode = Error.HelloFailedCode()
-        self.testMeterModFailedCode = Error.MeterModFailedCode()
-        self.testPortModFailedCode = Error.PortModFailedCode()
-        self.testQueueOpFailedCode = Error.QueueOpFailedCode()
-        self.testRoleRequestFailedCode = Error.RoleRequestFailedCode()
-        self.testSwitchConfigFailedCode = Error.SwitchConfigFailedCode()
-        self.testTableFeatureFailedCode = Error.TableFeaturesFailedCode()
-        self.testTableModFailedCode = Error.TableModFailedCode()
-        self.testBadPropertyCode = Error.BadPropertyCode()
-        self.testAsyncConfigFailedCode = Error.AsyncConfigFailedCode()
-        self.testFlowMonitorFailedCode = Error.FlowMonitorFailedCode()
-        self.testBundleFailedCode = Error.BundleFailedCode()
+        self.testGenericFailedCode = Error.GenericFailedCode
+        self.testBadActionCode = Error.BadActionCode
+        self.testBadInstructionCode = Error.BadInstructionCode
+        self.testBadMatchCode = Error.BadMatchCode
+        self.testBadRequestCode = Error.BadRequestCode
+        self.testErrorType = Error.ErrorType
+        self.testFlowModFailedCode = Error.FlowModFailedCode
+        self.testGroupModFailedCode = Error.GroupModFailedCode
+        self.testHelloFailedCode = Error.HelloFailedCode
+        self.testMeterModFailedCode = Error.MeterModFailedCode
+        self.testPortModFailedCode = Error.PortModFailedCode
+        self.testQueueOpFailedCode = Error.QueueOpFailedCode
+        self.testRoleRequestFailedCode = Error.RoleRequestFailedCode
+        self.testSwitchConfigFailedCode = Error.SwitchConfigFailedCode
+        self.testTableFeatureFailedCode = Error.TableFeaturesFailedCode
+        self.testTableModFailedCode = Error.TableModFailedCode
+        self.testBadPropertyCode = Error.BadPropertyCode
+        self.testAsyncConfigFailedCode = Error.AsyncConfigFailedCode
+        self.testFlowMonitorFailedCode = Error.FlowMonitorFailedCode
+        self.testBundleFailedCode = Error.BundleFailedCode
         self.testErrorMessage = Error.ErrorMsg()
         self.testErrorExperimenterMessage = Error.ErrorExperimenterMsg()
 
@@ -50,65 +50,210 @@ class TestErrorMessageTestCases(unittest.TestCase):
         pass
 
     def test_generic_failed_codeValue(self):
-        pass
+        value = 0
+        print()
+        print('Testing the Generic Failed Codes Value')
+        for elem in self.testGenericFailedCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_bad_action_codeValue(self):
-        pass
+        print()
+        print('Testing the Bad Action Codes Value')
+        value = 0
+
+        for elem in self.testBadActionCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
 
     def test_bad_instruction_codeValue(self):
-        pass
+        print()
+        print('Testing the Bad Instruction Codes Value')
+        value = 0
+
+        for elem in self.testBadInstructionCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_bad_match_codeValue(self):
-        pass
+        print()
+        print('Testing the Bad Match Codes Value')
+        value = 0
+
+        for elem in self.testBadMatchCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_bad_request_codeValue(self):
-        pass
+        print()
+        print('Testing the Bad Request Codes Value')
+        value = 0
+
+        for elem in self.testBadRequestCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_error_type(self):
-        pass
+        print()
+        print('Testing the Error Types Codes Value')
+        value = 0
+        temp = 0
+        for elem in self.testErrorType:
+
+            if elem == 0xffff:
+                temp = value
+                value = 0xffff
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
+            if elem == 0xffff:
+                value = temp + 1
 
     def test_flow_mod_failed_codeValue(self):
-        pass
+        print()
+        print('Testing the Flow Mod Failed Codes Value')
+        value = 0
+
+        for elem in self.testFlowModFailedCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_group_mod_failed_codeValue(self):
-        pass
+        print()
+        print('Testing the Group Mod Failed Codes Value')
+        value = 0
+
+        for elem in self.testGroupModFailedCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_hello_failed_codeValue(self):
-        pass
+        print()
+        print('Testing the Hello Failed Codes Value')
+        value = 0
+
+        for elem in self.testHelloFailedCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_meter_mod_failed_codeValue(self):
-        pass
+        print()
+        print('Testing the Meter Mod Failed Codes Value')
+        value = 0
+
+        for elem in self.testMeterModFailedCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_port_mod_failed_codeValue(self):
-        pass
+        print()
+        print('Testing the Port Mod Failed Codes Value')
+        value = 0
+
+        for elem in self.testPortModFailedCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_queue_op_failed_codeValue(self):
-        pass
+        print()
+        print('Testing the Queue Op Failed Codes Value')
+        value = 0
+
+        for elem in self.testQueueOpFailedCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_role_request_failed_codeValue(self):
-        pass
+        print()
+        print('Testing the Role Request Failed Codes Value')
+        value = 0
+
+        for elem in self.testRoleRequestFailedCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_switch_config_failed_codeValue(self):
-        pass
+        print()
+        print('Testing the Switch Config Failed Codes Value')
+        value = 0
+
+        for elem in self.testSwitchConfigFailedCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_table_feature_failed_codeValue(self):
-        pass
+        print()
+        print('Testing the Table Feature Failed Codes Value')
+        value = 0
+
+        for elem in self.testTableFeatureFailedCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_table_mod_failed_codeValue(self):
-        pass
+        print()
+        print('Testing the Table Mod Failed Codes Value')
+        value = 0
+
+        for elem in self.testTableModFailedCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_bad_property_codeValue(self):
-        pass
+        print()
+        print('Testing the Bad Property Codes Value')
+        value = 0
+
+        for elem in self.testBadPropertyCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_async_config_failed_codeValue(self):
-        pass
+        print()
+        print('Testing the Async Config Failed Codes Value')
+        value = 0
+
+        for elem in self.testAsyncConfigFailedCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_flow_monitor_failed_codeValue(self):
-        pass
+        print()
+        print('Testing the Flow Monitor Failed Codes Value')
+        value = 0
+
+        for elem in self.testFlowMonitorFailedCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_bundle_failed_codeValue(self):
-        pass
+        print()
+        print('Testing the Bundle Failed Codes Value')
+        value = 0
+
+        for elem in self.testBundleFailedCode:
+            print('Testing values {} against {}'.format(value, elem))
+            self.assertEqual(value, elem)
+            value += 1
 
     def test_error_message(self):
         pass
@@ -116,3 +261,8 @@ class TestErrorMessageTestCases(unittest.TestCase):
     def test_error_experimenter_message(self):
         pass
 
+
+
+
+if __name__ == '__main__':
+    unittest.main()
