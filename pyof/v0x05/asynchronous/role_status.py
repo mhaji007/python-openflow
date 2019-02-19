92dbdcfa528e553ef4a0b4d4703f581f3cb8e87d
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-02-19 15:39:38 -0500

    implements role_status in asynchronous

---
+++ pyof/v0x05/asynchronous/role_status.py
@@ -0,0 +1,60 @@
+
+from pyof.foundation.base import GenericMessage, GenericStruct
+from pyof.foundation.basic_types import BinaryData, FixedTypeList, UBInt16, UBInt8, UBInt32, UBInt64
+from pyof.v0x05.common.header import Type
+
+
+class OfpHeader(GenericStruct):
+    """Ofp Header"""
+
+
+    element_type = UBInt16()
+    content = BinaryData()
+
+    def __init__(self, message_type=None, content = b''):
+        """Create a Header with the optional parameters below.
+
+        Args:
+            element_type: One of OFPHET_*.
+        """
+        super().__init__()
+        self.message_type = message_type
+        self.content = content
+
+
+class OfpRolePropHeader(GenericStruct):
+    type = UBInt16()
+    length = UBInt16()
+
+    def __init__(self, type = None, length = None):
+        super().__init__()
+        self.type = type
+        self.length = length
+
+
+class OfpRoleStatus(GenericMessage):
+    """OpenFlow Hello Message OFPT_HELLO.
+
+    This message includes zero or more hello elements having variable size.
+    Unknown element types must be ignored/skipped, to allow for future
+    extensions.
+    """
+
+    header = OfpHeader(message_type=Type.OFPT_ROLE_STATUS)
+    role = UBInt32()
+    reason = UBInt8()
+    pad = FixedTypeList(UBInt8())
+    generation_id = UBInt64()
+
+    properties = FixedTypeList(OfpRolePropHeader())
+
+    def __init__(self, xid=None, data=None):
+        """Create a message with the optional parameters below.
+
+        Args:
+            xid (int): xid to be used on the message header.
+            elements: List of elements - 0 or more
+        """
+        super().__init__(xid)
+        self.data = data
+
