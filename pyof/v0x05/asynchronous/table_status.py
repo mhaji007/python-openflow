010da563f9d69694f8f480e4598f7a455cb9af0b
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-02-25 16:43:38 -0500

    Implement table-status in Asynchronous

---
+++ pyof/v0x05/asynchronous/table_status.py
@@ -0,0 +1,41 @@
+"""Defines an Table Status Message."""
+
+from pyof.foundation.base import GenericMessage, GenericStruct
+from pyof.foundation.basic_types import BinaryData, FixedTypeList, UBInt16, UBInt8, UBInt32, UBInt64, Pad
+from pyof.v0x05.common.header import Header, Type
+from pyof.v0x05.controller2Switch.table_description import TableDescription
+
+
+class TableReason(IntEnum):
+    """What changed about the table."""
+
+    #: Vacancy down threshold event
+    OFPTR_VACANCY_DOWN = 3
+    #: Vacancy up threshold event
+    OFPTR_VACANCY_UP = 4
+
+
+
+class TableStatus(GenericMessage):
+    """OpenFlow TableStatus Message OFPT_TABLE_STATUS.
+    """
+
+    header = Header(message_type=Type.OFPT_TABLE_STATUS)
+    reason = UBInt8()
+
+   # pad = FixedTypeList(UBInt8())
+    pad = Pad(7)
+    generation_id = UBInt64()
+
+    table = TableDescription()
+
+    def __init__(self, xid=None, data=None):
+        """Create a message with the optional parameters below.
+
+        Args:
+            xid (int): xid to be used on the message header.
+            elements: List of elements - 0 or more
+        """
+        super().__init__(xid)
+        self.data = data
+
