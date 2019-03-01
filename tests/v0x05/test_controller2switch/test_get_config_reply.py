ce5f7131fe9269d379374819c6d48e0473564fe7
Roicxy Alonso <roicxyster@gmail.com>
2019-02-28 21:09:21 -0500

    Added a class to the test_get_config_reply

--- tests/v0x05/test_controller2switch/test_get_config_reply.py
+++ tests/v0x05/test_controller2switch/test_get_config_reply.py
@@ -1,15 +1,35 @@
 """Config Reply message tests."""
 from pyof.v0x05.controller2switch.get_config_reply import GetConfigReply
 from tests.test_struct import TestStruct
+import unittest
 
 
-class TestGetConfigReply(TestStruct):
-    """Config Reply message tests."""
 
-    @classmethod
-    def setUpClass(cls):
-        """Configure raw file and its object in parent class (TestDump)."""
-        super().setUpClass()
-        super().set_raw_dump_file('v0x05', 'ofpt_get_config_reply')
-        super().set_raw_dump_object(GetConfigReply, xid=1)
-        super().set_minimum_size(12)
+
+class TestFakeSwitch():
+
+
+
+    def __int__(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+# class TestGetConfigReply(TestStruct):
+#     """Config Reply message tests."""
+#
+#     @classmethod
+#     def setUpClass(cls):
+#         """Configure raw file and its object in parent class (TestDump)."""
+#         super().setUpClass()
+#         super().set_raw_dump_file('v0x05', 'ofpt_get_config_reply')
+#         super().set_raw_dump_object(GetConfigReply, xid=1)
+#         super().set_minimum_size(12)
