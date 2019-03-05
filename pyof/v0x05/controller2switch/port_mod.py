1c9c9c7db6d0b84e9a0b841f3f6d9225ebbe830b
Roicxy Alonso <roicxyster@gmail.com>
2019-03-04 22:25:34 -0500

    Added some new structures and enums classes

--- pyof/v0x05/controller2switch/port_mod.py
+++ pyof/v0x05/controller2switch/port_mod.py
@@ -5,32 +5,58 @@
 # Third-party imports
 
 # Local source tree imports
-from pyof.foundation.base import GenericMessage
-from pyof.foundation.basic_types import HWAddress, Pad, UBInt32
+from pyof.foundation.base import GenericMessage, IntEnum, GenericStruct
+from pyof.foundation.basic_types import HWAddress, Pad, UBInt32, FixedTypeList, UBInt16
 from pyof.v0x05.common.header import Header, Type
 from pyof.v0x05.common.port import PortConfig, PortFeatures
 
-__all__ = ('PortMod',)
+__all__ = ('PortMod','PortModPropType', 'PortModPropHeader', )
+
+#: Enums
+class PortModPropType(IntEnum):
+    """Port mod property types"""
+
+    #: Ethernet property.
+    OFPPMPT_ETHERNET = 0
+    #: Optical property.
+    OFPPMPT_OPTICAL = 1
+    #: Experimenter property.
+    OFPPMPT_EXPERIMENTER = 0xffff
 
 # Classes
+
+class PortModPropHeader(GenericStruct):
+    """Common header for all port mod properties."""
+
+    #: One of OFPPMPT_*
+    type = UBInt16()
+    #: Length in bytes of this property.
+    length = UBInt16()
 
 
 class PortMod(GenericMessage):
     """Implement messages to modify the physical port behavior."""
 
     header = Header(message_type=Type.OFPT_PORT_MOD)
+
     port_no = UBInt32()
     pad = Pad(4)
+    #: The hardware address is not configurable. This is used to
+    #: sanity-check the request, so it must be the same as returned in an port struct.
     hw_addr = HWAddress()
+    #: Pad to 64 bits.
     pad2 = Pad(2)
+    #: Bitmap of OFPPC_* flags.
     config = UBInt32(enum_ref=PortConfig)
+    #: Bitmap of OFPPC_* flags to be changed.
     mask = UBInt32(enum_ref=PortConfig)
-    advertise = UBInt32(enum_ref=PortFeatures)
-    #: Pad to 64-bits.
-    pad3 = Pad(4)
+    #: Port mod property list - 0 or more properties
+    properties = FixedTypeList(pyof_class=PortModPropHeader)
+
+
 
     def __init__(self, xid=None, port_no=None, hw_addr=None, config=None,
-                 mask=None, advertise=None):
+                 mask=None, properties=FixedTypeList(pyof_class=PortModPropHeader)):
         """Create a PortMod with the optional parameters below.
 
         Args:
@@ -43,13 +69,25 @@ class PortMod(GenericMessage):
                 Bitmap of OFPPC_* flags
             mask (~pyof.v0x05.common.port.PortConfig):
                 Bitmap of OFPPC_* flags to be changed
-            advertise (~pyof.v0x05.common.port.PortFeatures):
-                Bitmap of OFPPF_*. Zero all bits to prevent any action taking
-                place.
+            properties (FixedTypeList(pyof_class=PortModPropHeader)):
+                Port mod property list - 0 or more properties
         """
         super().__init__(xid)
         self.port_no = port_no
         self.hw_addr = hw_addr
         self.config = config
         self.mask = mask
+        self.properties = properties
+
+class PortModPropEthernet(PortModPropHeader):
+    """Ethernet port mod property"""
+
+    #: Bitmap of OFPPF_*. Zero all bits to prevent any action taking place.
+    advertise = UBInt32()
+
+
+    def __init__(self, advertise=None):
+        super().type = PortModPropType.OFPPMPT_ETHERNET
         self.advertise = advertise
+        super().length = self.__sizeof__()
+
