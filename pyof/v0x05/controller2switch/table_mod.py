4eb4b28d64af8bacfe1246b41dc0a59d95177206
Mehdi Hajikhani <mhaji007@fiu.edu>
2019-03-07 16:28:59 -0500

    Add/fix comments Add/fix docstrings Add properties field to TableMod class and constructor

--- pyof/v0x05/controller2switch/table_mod.py
+++ pyof/v0x05/controller2switch/table_mod.py
@@ -1,13 +1,20 @@
-"""Flow Table Modification message."""
+""" Defines Flow Table Modification message."""
+
+# System imports
+
 from enum import IntEnum
 
 from pyof.foundation.base import GenericBitMask, GenericMessage, GenericStruct
 from pyof.foundation.basic_types import Pad, UBInt8, UBInt32, UBInt16, FixedTypeList
 from pyof.v0x05.common.header import Header, Type
 
+# Third-party imports
 
-__all__ = ('Table', 'TableMod', 'TableModPropType', 'TableModPropHeader', 'TableConfig', 'TableModPropEviction','TableModPropEvictionFlag')
+__all__ = ('Table', 'TableMod', 'TableModPropType', 'TableModPropHeader', 'TableConfig', 'TableModPropEviction',
+           'TableModPropEvictionFlag')
 
+
+# Enums
 
 
 class Table(IntEnum):
@@ -20,7 +27,6 @@ class Table(IntEnum):
     OFPTT_ALL = 0xff
 
 
-
 class TableModPropType(IntEnum):
     """Table Mod property types"""
 
@@ -30,6 +36,9 @@ class TableModPropType(IntEnum):
     OFPTMPT_VACANCY = 0x3
     #: Experimenter
     OFPTMPT_EXPERIMENTER = 0xFFFF
+
+
+# Classes
 
 
 class TableModPropEvictionFlag(GenericBitMask):
@@ -72,48 +81,87 @@ class TableModPropEviction(GenericStruct):
     """Eviction table mod Property. Mostly used in OFPMP_TABLE_DESC replies"""
 
     #: OFPTMPT_EVICTION
-    type = UBInt16(TableModPropType.OFPTMPT_EVICTION)
+    type = UBInt16()
     #: Length in bytes of this property
     length = UBInt16()
     #: Bitmap of OFPTMPEF_* flags
     flags = UBInt32()
 
+    def __init__(self, type=TableModPropType.OFPTMPT_EVICTION, length=None, flags=None):
+        super().__init__()
+        self.type = type
+        self.length = length
+        self.flags = flags
 
 
 class TableModPropVacancy(GenericStruct):
     """Vacancy table mod property"""
 
     #: OFPTMPT_VACANCY
-    type = UBInt16(TableModPropType.OFPTMPT_VACANCY)
+    type = UBInt16()
     #: Length in bytes of this property
     length = UBInt16()
     #: Vacancy threshold when space decreases (%)
-    vacancy_down=UBInt8()
+    vacancy_down = UBInt8()
     #: Vacancy threshold when space increases (%)
-    vacancy_up=UBInt8()
+    vacancy_up = UBInt8()
     #: Current vacancy (%) - only in ofp_table_desc
-    vacancy=UBInt8()
+    vacancy = UBInt8()
     #: Align to 64 bits
     pad = Pad(1)
 
+    def __init__(self, type=TableModPropType.OFPTMPT_VACANCY, length=None, vacancy_down=None, vacancy_up=None,
+                 vacancy=None):
+        super().__init__()
+        self.type = type
+        self.length = length
+        self.vacancy_down = vacancy_down
+        self.vacancy_up = vacancy_up
+        self.vacancy = vacancy
 
 
+class TableModPropExperimenter(GenericStruct):
+    """Experimenter table mod property"""
 
+    #: OFPTMPT_EXPERIMENTER
+    type = UBInt16(TableModPropType.OFPTMPT_EXPERIMENTER)
+    #: Length in bytes of this property
+    length = UBInt16()
+    #: Experimenter ID which takes the same
+    # form as in struct
+    # ofp_experimenter_header.
+    experimenter = UBInt32()
+    #: Experimenter defined
+    exp_type = UBInt32()
+    #: Followed by: Exactly (length - 12) bytes containing the experimenter data, then
+    # Exactly (length + 7)/8*8 - (length) (between 0 and 7)
+    # bytes of all-zero bytes
+    experimenter_data = UBInt32()
 
+    def __int__(self, type=Type.OFPRPT_EXPERIMENTER, length=None, experimenter=None, exp_type=None,
+                experimenter_data=None):
+        self.type = type
+        self.length = length
+        self.experimenter = experimenter
+        self.exp_type = exp_type
+        self.experimenter_data = experimenter_data
 
 
 class TableMod(GenericMessage):
     """Configure/Modify behavior of a flow table."""
 
+    #: class:`~pyof.v0x05.common.action.ActionHeader`: OpenFlow Header
     header = Header(message_type=Type.OFPT_TABLE_MOD)
+    #: ID of the table, OFPTT_ALL indicates all tables
     table_id = UBInt8()
     #: Pad to 32 bits
     pad = Pad(3)
+    #: Bitmap of OFPTC_* flags
     config = UBInt32()
-
+    #: Table Mod Property list
     properties = FixedTypeList(TableModPropHeader)
 
-    def __init__(self, xid=None, table_id=Table.OFPTT_ALL, config=3):
+    def __init__(self, xid=None, table_id=Table.OFPTT_ALL, config=3, properties=None):
         """Assing parameters to object attributes.
 
         Args:
@@ -128,3 +176,4 @@ class TableMod(GenericMessage):
         # This is reserved for future used. The default value is the only valid
         # one from the Enum.
         self.config = config
+        self.properties = properties
