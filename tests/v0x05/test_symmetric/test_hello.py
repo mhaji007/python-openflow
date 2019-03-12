99d758ac98a41ae854c18f4adc546106f2bb8ca3
Roicxy Alonso <roicxyster@gmail.com>
2019-03-11 21:56:28 -0400

    Changed comments and structure of the test functions.

--- tests/v0x05/test_symmetric/test_hello.py
+++ tests/v0x05/test_symmetric/test_hello.py
@@ -8,101 +8,106 @@ from pyof.foundation.basic_types import UBInt32, UBInt16, TypeList, BinaryData
 
 class TestHello(unittest.TestCase):
 
-    def test_helloVersionBitmap(self):
+    def test_hello_version_bitmap(self):
+        """
+        Test if the bitmap version is 1
 
-        # Test if the bitmap version is 1
+        :return: None
+        """
+
         self.assertEqual(1, Hello.HelloElemType.OFPHET_VERSIONBITMAP)
 
+    def test_hello_elem_header(self):
+        """
+        Testing the HelloElemHeader one element.
 
-    def test_helloElemHeader(self):
+        :return: None
+        """
 
-        print()
-        print('Testing the HelloElemHeader one element')
-        testValue = b'\x00\x01\x00\x04'
+        test_value = b'\x00\x01\x00\x04'
 
-        self.testObjectElemHeader = Hello.HelloElemHeader(Hello.HelloElemType.OFPHET_VERSIONBITMAP, 4)
-        value = self.testObjectElemHeader.pack()
+        self.test_object_elem_header = Hello.HelloElemHeader(Hello.HelloElemType.OFPHET_VERSIONBITMAP, 4)
+        value = self.test_object_elem_header.pack()
 
-        print('Testing the values fix value {} and given value from the header {}'.format(testValue, value))
+        self.assertEqual(test_value,value)
 
-        self.assertEqual(testValue,value)
+    def test_hello_list_of_hello_elements(self):
+        """
+        Testing the HelloElemHeader more than one element.
+        Testing the pack value return by the ListOfHelloElements more than one element.
+        :return: None
+        """
 
+        test_value = b'\x00\x01\x00\x04\x00\x01\x00\x04\x00\x01\x00\x04'
 
-    def test_helloListOfHelloElements(self):
-        print()
-        print('Testing the HelloElemHeader more than one element')
-        testValue = b'\x00\x01\x00\x04\x00\x01\x00\x04\x00\x01\x00\x04'
+        self.test_object_list_of_hello_elem = Hello.ListOfHelloElements()
 
-        self.testObjectListOfHelloElem = Hello.ListOfHelloElements()
-
-        self.testListOfElem = [Hello.HelloElemHeader(Hello.HelloElemType.OFPHET_VERSIONBITMAP,4),
+        self.test_list_of_elem = [Hello.HelloElemHeader(Hello.HelloElemType.OFPHET_VERSIONBITMAP,4),
                                Hello.HelloElemHeader(Hello.HelloElemType.OFPHET_VERSIONBITMAP,4),
                                Hello.HelloElemHeader(Hello.HelloElemType.OFPHET_VERSIONBITMAP,4)]
 
-        self.testObjectListOfHelloElem.__init__(self.testListOfElem)
+        self.test_object_list_of_hello_elem.__init__(items=self.test_list_of_elem)
 
-        val = self.testObjectListOfHelloElem.pack()
+        val = self.test_object_list_of_hello_elem.pack()
 
-        print('Testing the pack value return by the ListOfHelloElements more than one element.')
-        print('Testing fix value {} versus given value {}'.format(testValue, val))
+        self.assertEqual(test_value, val)
 
-        self.assertEqual(testValue, val)
+    def test_hello_list_of_hello_element(self):
+        """
+        Testing the HelloElemHeader one element.
+        Testing the pack value return by the ListOfHelloElements one element.
 
-
-    def test_helloListOfHelloElement(self):
+        :return: None
+        """
         print()
-        print('Testing the HelloElemHeader one element')
-        testValue = b'\x00\x01\x00\x04'
-        self.testObjectListOfHelloElem = Hello.ListOfHelloElements()
+        print('')
+        test_value = b'\x00\x01\x00\x04'
+        self.test_object_list_of_hello_elem = Hello.ListOfHelloElements()
 
-        self.testObjectListOfHelloElem.__init__(Hello.HelloElemHeader(Hello.HelloElemType.OFPHET_VERSIONBITMAP,4))
+        self.test_object_list_of_hello_elem.__init__(Hello.HelloElemHeader(Hello.HelloElemType.OFPHET_VERSIONBITMAP,4))
 
+        val = self.test_object_list_of_hello_elem.pack()
 
-        val = self.testObjectListOfHelloElem.pack()
-
-        print('Testing the pack value return by the ListOfHelloElements one element.')
-        print('Testing fix value {} versus given value {}'.format(testValue, val))
-
-        self.assertEqual(testValue, val)
+        self.assertEqual(test_value, val)
 
     def test_hello(self):
+        """
+        Testing the Hello Message pack.
+
+        :return: None
+        """
 
         _NUM_OF_TEST = 25
-        print()
-        print('Testing the Hello Message pack.')
 
         for i in range(0, _NUM_OF_TEST):
             random.seed()
 
-            testVal = i + random.randint(0,100)
-            testValue = b'\x05\x00\x00\x08'+ UBInt32(testVal).pack()
-            self.testObjectHello = Hello.Hello(testVal)
+            test_val = i + random.randint(0,100)
+            test_value = b'\x05\x00\x00\x08'+ UBInt32(test_val).pack()
+            self.test_object_hello = Hello.Hello(test_val)
 
-            val = self.testObjectHello.pack()
+            val = self.test_object_hello.pack()
+            self.assertEqual(test_value, val)
 
-            print('Testing the fix value message {} versus the given value {}'.format(testValue, val))
-            self.assertEqual(testValue, val)
+    def test_hello_elem_version_bitmap(self):
+        """
+        Testing the class HelloElemVersionBitmap.
+        Test support 2 versions (ver 1 = 0x01 and ver 1.3 = 0x04)
+        Testing 2 version support ver 1 and ver 1.3 based on the specification's example.
 
-
-
-
-    def test_helloElemVersionBitmap(self):
-        print()
-        print('Testing the class HelloElemVersionBitmap.')
-        """ Test support 2 versions (ver 1 = 0x01 and ver 1.3 = 0x04) """
+        :return: None
+        """
 
         ver1 = 0x01
         ver3 = 0x04
         ver = ver1 << ver3
         ver = ver | 2
 
-        testVal = b'\x00\x01\x00\x08\x00\x00\x00\x12'
-        self.testObjectHelloElemVersion = Hello.HelloElemVersionBitmap(UBInt16(8), UBInt32(ver).pack())
-        val = self.testObjectHelloElemVersion.pack()
-        print('Testing 2 version support ver 1 and ver 1.3 based on the specification\'s example.')
-        print('Testing fix value {} versus given value {}.'.format(testVal, val))
+        test_val = b'\x00\x01\x00\x08\x00\x00\x00\x12'
+        self.test_object_hello_elem_version = Hello.HelloElemVersionBitmap(UBInt16(8), UBInt32(ver).pack())
+        val = self.test_object_hello_elem_version.pack()
 
-        self.assertEqual(testVal,val)
+        self.assertEqual(test_val,val)
 
 
 
